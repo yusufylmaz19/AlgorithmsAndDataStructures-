684a3338ecfcc7266c7082d4938f43f1c100e5d8
Yusuf <yusufylmaz4587@gmail.com>
2021-12-08 15:21:17 +0300

    first commit git push -u origin main

---
+++ SearchingAlgorithms/binarySearch.py
@@ -0,0 +1,32 @@
+def binary_serach(list,target):
+    first=0
+    last=len(list)-1
+
+    while first<=last:
+        midpoint=(first+last)//2
+
+        if list[midpoint]==target:
+            return midpoint
+        elif list[midpoint]<target:
+            first=midpoint+1
+        else:
+            last=midpoint-1
+    
+    return None        
+         
+def verify(index):
+    if index is not None:
+       print("Target found at index:",index)
+    else:
+       print("Target not found in list")
+
+
+numbers=[0,1,2,3,4,5,6,7,9,10]     # lis has to be sorted
+ 
+result= binary_serach(numbers,12)
+verify(result)
+result= binary_serach(numbers,1)
+verify(result)
+
+
+    # wosrt case is O(logn)
---
+++ SearchingAlgorithms/linearSearch.py
@@ -0,0 +1,26 @@
+def linear_search(list,target):
+    """
+    retruns the index position of the target,if found else returns None 
+    """
+    for i in range(0,len(list)):
+        if list[i]==target:
+         return i
+    return None
+
+
+def verify(index):
+    if index is not None:
+       print("Target found at index:",index)
+    else:
+       print("Target not found in list")
+
+numbers=[0,1,2,3,4,5,6,7,9,10]    # lis has to be sorted
+
+result= linear_search(numbers,12)
+verify(result)
+result= linear_search(numbers,1)
+verify(result)
+
+
+    # worst case is O(n)
+   
