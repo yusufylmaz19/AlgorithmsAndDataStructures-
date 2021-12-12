7cffb655538e1df289f733ed1ba4cc9f0c05c478
Yusuf <yusufylmaz4587@gmail.com>
2021-12-12 15:54:26 +0300

    commit

---
+++ sortingAlgorithms/bogo_sort.py
@@ -0,0 +1,24 @@
+import random
+
+import sys
+
+from load import  load_numbers
+
+numbers = load_numbers(sys.argv[1])
+
+def is_sorted(values):
+    for index in range (len(values)-1):
+        if values[index]> values[index+1]:
+            return False
+    return True
+
+
+def bogo_sort(values):
+    attempts=0
+    while not is_sorted(values):
+       print(attempts)
+       random.shuffle(values)
+       attempts+=1
+    return values
+
+print(bogo_sort(numbers))    
